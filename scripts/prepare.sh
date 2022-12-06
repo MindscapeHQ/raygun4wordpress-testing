77aa52bf5578dc7902875fe1417fc74ef800ff72
Jared Scholz <jaredsc108@gmail.com>
2022-12-07 11:45:17 +1300

    Cleaned up and started implementing automated submodule updating

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,3 +6,7 @@ echo "prepare.sh running"
 wp core install --path="/var/www/html" --url="http://localhost:8000" --title="LocalWordPress" --admin_user=raygun --admin_password=raygunadmin --admin_email=jared@raygun.com
 # Scaffold the raygun-testing plugin if not already done so:
 [ ! -d "/var/www/html/wp-content/plugins/raygun-testing" ] && mkdir -p /var/www/html/wp-content/plugins/raygun-testing && wp scaffold plugin raygun-testing
+# TODO: move to a submodule update script ran by the git container:
+# Update the raygun4wordpress plugin Git submodule:
+cd /repository
+git submodule set-branch -b $raygun4wordpress_branch
