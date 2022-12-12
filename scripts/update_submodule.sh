7b0b93975d46a0f4e6243a68223332d6fecc2108
Jared Scholz <jaredsc108@gmail.com>
2022-12-13 11:45:14 +1300

    Added clean command and settings file

--- scripts/update_submodule.sh
+++ scripts/update_submodule.sh
@@ -4,17 +4,18 @@ cd /repository
 
 echo "Cleaning raygun-testing"
 for file in raygun4wordpress/* raygun4wordpress/.*; do
-  [ -e raygun-testing/"$file" ] || continue
   file="$(basename -- $file)"
   case "$file" in "."|"..") continue ;; esac
   rm -rf raygun-testing/"$file"
 done
 
-echo "Updating raygun4wordpress submodule"
-git submodule--helper set-branch -b ${PROVIDER_BRANCH} raygun4wordpress
-git submodule update --init --remote
+if [ "${UPDATE}" == "true" ]; then
+  echo "Updating raygun4wordpress submodule from branch \"${PROVIDER_BRANCH}\""
+  git submodule--helper set-branch -b ${PROVIDER_BRANCH} raygun4wordpress
+  git submodule--helper update --init --remote
 
-echo "Merging raygun4wordpress into raygun-testing"
-for file in raygun4wordpress/*; do
-  cp -rf "$file" raygun-testing
-done
+  echo "Merging raygun4wordpress into raygun-testing (file copy)"
+  for file in raygun4wordpress/*; do
+    cp -r "$file" raygun-testing
+  done
+fi
