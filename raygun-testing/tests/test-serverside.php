99466a5cc36afaef8642df150c5f3f764e4ec6e9
Jared Scholz <jaredsc108@gmail.com>
2022-12-19 17:02:37 +1300

    Small quality of life changes

--- raygun-testing/tests/test-serverside.php
+++ raygun-testing/tests/test-serverside.php
@@ -1,39 +1,42 @@
 <?php
 
+/*
+ * NOTE: Testing Raygun4WP version 1.9 requires a slight modification to its main.php.
+ * Global variables must be declared explicitly within PHPUnit.
+ * This means that initialization of the RaygunClient (approx. line 169) must follow "global $client;"
+ */
+
 class ServersideTest extends WP_UnitTestCase
 {
 
-    public function testUncaughtException()
+    public function testErrorHandlerManually()
     {
-        trigger_error("TEST MANUAL ERROR"); // TODO: this will only work if the global raygun client variable is declared correctly!
-        /*if (get_option('rg4wp_status') && function_exists('curl_version') && get_option('rg4wp_apikey')) {
-            require_once '/test_plugin/external/raygun4php/src/Raygun4php/RaygunClient.php';
-            $client = new Raygun4php\RaygunClient(get_option('rg4wp_apikey'), false, true);
-
-            if (get_option('rg4wp_usertracking')) {
-                $client->SetUser(get_option('user'));
-            }
-
-            $result = trim($client->SendError(404, 'Congratulations, Raygun4WP is working correctly!', '0', '0'));
-            if ($result == 'HTTP/1.1 403 Forbidden') {
-                echo 'The Raygun service did not accept your API key. Please check to see you have a entered a valid API key for an application and then save your changes.';
-            } else if ($result == 'HTTP/1.1 202 Accepted') {
-                echo 'Raygun has accepted the test issue. Check your <a href="http://app.raygun.com" target="_blank">dashboard</a> to see the issue details!';
-            } else {
-                echo 'Woops, the errors status was not reported. Check your <a href="http://app.raygun.com" target="_blank">dashboard</a> to see if your error has been reported. If the error doesn\'t appear make sure you have entered a valid API key for an application you have created and then try again.';
-            }
-        } else {
-            echo 'Something is missing! Please check that you have enabled Serverside error tracking, the API key is pasted in and you have saved the settings.';
-        }*/
-        $this->assertTrue(true);
+        $this->expectNotToPerformAssertions();
+        trigger_error("Error handler functions with trigger_error");
     }
 
-    /* TODO: post settings to the setting page and confirm with get_options
-    // https://stackoverflow.com/questions/70006997/access-host-from-within-a-docker-container
-    public function testSettingsPage()
+    /*public function testSettingsPagePost()
     {
+        $this->expectNotToPerformAssertions();
         exec("curl -X POST -d \"option_page=rg4wp&action=update&_wpnonce=" . wp_create_nonce() . "&_wp_http_referer=%2Fwp-admin%2Fadmin.php%3Fpage%3Drg4wp-settings&rg4wp_apikey=" . getenv("API_KEY") . "&rg4wp_ignoredomains=&rg4wp_usertracking=1&rg4wp_status=1&rg4wp_js=1&rg4wp_404s=1&rg4wp_async=1&rg4wp_tags=&rg4wp_js_tags=&rg4wp_pulse=1&action=update&page_options=rg4wp_status%2Crg4wp_apikey%2Crg4wp_tags%2Crg4wp_404s%2Crg4wp_js%2Crg4wp_usertracking%2Crg4wp_ignoredomains%2Crg4wp_pulse%2Crg4wp_js_tags&submitForm=Save+Changes\" https://host.docker.internal:8000/wp-admin/options.php");
         exec("curl -X POST -d \"page=rg4wp-settings&settings-updated=true\" https://host.docker.internal:8000/wp-admin/admin.php");
-        $this->assertTrue(true);
     }*/
+
+    public function testSendTestErrorPost()
+    {
+        $this->expectNotToPerformAssertions();
+        $response = $this->post('/sendtesterror.php', [
+            'rg4wp_status' => get_option('rg4wp_status'),
+            'rg4wp_apikey' => get_option('rg4wp_apikey'),
+            'rg4wp_usertracking' => get_option('rg4wp_usertracking'),
+            'user' => 'testrunner'
+        ]);
+        print $response;
+    }
+
+    public static function tear_down_after_class()
+    {
+        print "\nRESULTS MUST NOW BE VERIFIED WITHIN THE RAYGUN APPLICATION: SEE README";
+        print "\nASSUMING NO FAILURES BELOW:";
+    }
 }
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,14 +3,19 @@
 # Wait for database initialization
 sleep 1
 while ! mysqladmin ping -h db --silent; do
-  echo "Waiting for database"
+  echo "(Waiting for database)"
   sleep 1
 done
 
 cd /test_plugin
 if [ -f raygun4wp.php ]; then
-  yes | bin/install-wp-tests.sh wordpress_test root wordpress db:3306 latest
-  composer run test
+  if [ "${API_KEY}" != "<API key here>" ]; then
+    # If a specific version of WordPress needs to be tested, change it here \/
+    yes | bin/install-wp-tests.sh wordpress_test root wordpress db:3306 latest
+    composer run test
+  else
+    echo "Enter your API key in settings.env!"
+  fi
 else
   echo "raygun4wp missing: did you run \"docker compose --profile update up\"?"
 fi
