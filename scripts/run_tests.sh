26a4626043110bda0852eb8ec224f41f75d5c4a8
Jared Scholz <jaredsc108@gmail.com>
2022-12-21 12:06:17 +1300

    Adding wp-cli container to docker-compose

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,8 @@
 #!/etc/env bash
 
-# Wait for database initialization
-sleep 1
-while ! mysqladmin ping -h db --silent; do
-  echo "(Waiting for database)"
-  sleep 1
-done
-
 if [ "${API_KEY}" != "<API key here>" ]; then
   echo "Installing WordPress Core"
-  wp core install --path="/var/www/html" --url="http://localhost:8000" --title="WPLOCAL" --admin_user=raygun --admin_password=raygunadmin --admin_email=test@raygun.com
+  wp core install --path="/var/www/html" --url="http://localhost:8000" --title="wplocal" --admin_user=raygun --admin_password=raygunadmin --admin_email=test@raygun.com
 else
   echo "Enter your API key in settings.env!"
 fi
