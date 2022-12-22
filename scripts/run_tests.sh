00c5ae3a354071df67fa87e06ccb1b2b6c8afad9
Jared Scholz <jaredsc108@gmail.com>
2022-12-22 15:52:59 +1300

    Moved wordpress healthcheck into run_tests.sh
    Additionally added some documentation

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,20 @@
 #!/etc/env bash
 
+# wordpress container health check values:
+interval=1 # Seconds
+retries=20
+timeout=5 # Seconds
+
+# Wait for the wordpress server:
+sleep 2 # Give initial setup time
+connected=0
+for i in $(seq $retries); do
+  sleep $interval
+  curl -m $timeout -s --fail wordpress:80
+  [ $? -eq 0 ] && connected=1 && break
+done
+[ $connected -eq 0 ] && echo "Could not connect to wordpress!" && exit 1
+
 if [ "${API_KEY}" != "<API key here>" ]; then
   echo "Installing WordPress Core"
   wp core install --path="/var/www/html" --url="http://wordpress:80" --title="wordpress" --admin_user=raygun --admin_password=raygunadmin --admin_email=test@raygun.com
