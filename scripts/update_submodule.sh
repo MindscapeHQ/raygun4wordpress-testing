bba3ed60aeeb9f56ec0168a93f8ddd4cae00d7ea
Jared Scholz <jaredsc108@gmail.com>
2023-01-19 16:07:18 +1300

    Fixed submodule updating

--- scripts/update_submodule.sh
+++ scripts/update_submodule.sh
@@ -1,7 +1,7 @@
 #!/etc/env ash
 
-cd /repository
 echo "Updating raygun4wordpress submodule from branch \"${PROVIDER_BRANCH}\""
-#git submodule add https://github.com/MindscapeHQ/raygun4wordpress plugins/raygun4wordpress
+cd /repository
+# git submodule add https://github.com/MindscapeHQ/raygun4wordpress plugins/raygun4wordpress
 git submodule--helper set-branch -b ${PROVIDER_BRANCH} plugins/raygun4wordpress
-git submodule--helper update --init --recursive
+git submodule update --init --recursive --remote --merge
